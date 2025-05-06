6b02871c43cf66c088491a8ba14c1336f86b886c
Iqblpra_ <iibrrot.15@gmail.com>
2025-05-06 10:09:22 +0700

    Perubahan tabel

--- table.php
+++ table.php
@@ -4,57 +4,84 @@ include 'database.php';
 function select($query)
 {
     global $db;
-
     $result = mysqli_query($db, $query);
     $rows = [];
 
     while ($row = mysqli_fetch_assoc($result)) {
         $rows[] = $row;
     }
-    return $rows; // Kembalikan array, bukan result object
+    return $rows;
 }
 
-$data_barang = select("SELECT * FROM barang");
+$data_barang = select("SELECT * FROM produk");
 ?>
 
 <?php include "layout/header.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Data Barang</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    </style> 
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/table.css">
+</head>
+<body>
+
 
 <!-- Table Start -->
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
         <div class="col-12">
             <div class="bg-secondary rounded h-100 p-4">
-                <h6 class="mb-4">Responsive Table</h6>
+                <h6 class="text-center">DATA BARANG</h6>
+                
                 <div class="table-responsive">
                     <table class="table">
                         <thead>
                             <tr>
                                 <th scope="col">No</th>
-                                <th scope="col">Nama</th>
-                                <th scope="col">Jumlah</th>
-                                <th scope="col">Harga</th>
-                                <th scope="col">Tanggal</th>
+                                <th scope="col">Nama Produk</th>
+                                <th scope="col">Kategori</th>
+                                <th scope="col">Stok</th>
+                                <th scope="col">Harga Jual</th>
+                                <th scope="col">Harga Beli</th>
+                                <th scope="col">Supplier</th>
+                                <th scope="col">Tanggal Masuk</th>
                                 <th scope="col">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php $no = 1; ?>
-                            <?php foreach ($data_barang as $barang) : ?>
+                            <?php foreach ($data_barang as $produk) : ?>
                                 <tr>
                                     <td><?= $no++; ?></td>
-                                    <td><?= $barang['nama']; ?></td>
-                                    <td><?= $barang['jumlah']; ?></td>
-                                    <td><?= $barang['harga']; ?></td>
-                                    <td><?= $barang['tanggal']; ?></td>
+                                    <td><?= $produk['nama']; ?></td>
+                                    <td><?= $produk['kategori']; ?></td>
+                                    <td><?= $produk['stok']; ?></td>
+                                    <td><?= $produk['harga_beli']; ?></td>
+                                    <td><?= $produk['harga_jual']; ?></td>
+                                    <td><?= $produk['supplier']; ?></td>
+                                    <td><?= $produk['tgl_masuk']; ?></td>
                                     <td width="15%" class="text-center">
-                                        <button type="button" class="btn btn-success">Edit</button>
-                                        <button type="button" class="btn btn-danger">Hapus</button>
+                                        <button type="button" class="btn-edit">
+                                            Edit
+                                        </button>
+                                        <button type="button" class="btn-delete">
+                                            Hapus
+                                        </button>
                                     </td>
                                 </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
+
             </div>
         </div>
     </div>
