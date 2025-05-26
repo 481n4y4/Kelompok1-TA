c90f8b1047e2723647a97d9e4c26e9c548172d56
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-05-26 13:02:45 +0700

    memperbaiki navbar

--- layout/header.php
+++ layout/header.php
@@ -67,7 +67,7 @@ require_once 'config/controller.php';
         <!-- Content Start -->
         <div class="content">
             <!-- Navbar Start -->
-            <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
+            <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-3">
                 <a href="index.php" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
                 </a>
